a286cf59e52e90b7438bd74bbc386cd3095b2a5d
Khaoula Bahloul <khaoula@uw.edu>
2019-09-22 21:52:57 -0700

    Create a practice project and add solution for ReverseNumber problem

---
+++ C#/Practice1/Practice1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+namespace Practice1
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+            
+    //    }
+    //}
+
+    public class Solution
+    {
+        public static void Main (string[] args)
+        {
+            int result = Reverse(1234567899);
+            Console.WriteLine(result);
+      
+        }
+
+       
+
+        
+
+        public bool IsPalindrome(int x)
+        {
+            bool result = false;
+            return result;
+        }
+    }
+}
---
+++ C#/Practice1/Practice1/ReverseNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice1
+{
+    public class ReverseNumber
+    {
+        //Given a 32-bit signed integer, reverse digits of an integer.
+        public static int Reverse(int x)
+        {
+            string output = "";
+            int quotient = x;
+            int remainder = 0;
+            int result = 0;
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            if (x < 0)
+            {
+                output = "-";
+                quotient = quotient * -1;
+            }
+            while (quotient / 10 != 0)
+            {
+                remainder = quotient % 10;
+                quotient = quotient / 10;
+                output = output + remainder;
+            }
+            output = output + quotient;
+
+            try
+            {
+                result = checked(Convert.ToInt32(output));
+            }
+            catch
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
